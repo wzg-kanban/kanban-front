cb8b0e39b54dd2b42920e54f782f56a01907bc14
kiper <kacgrzes@gmail.com>
2016-07-31 18:15:44 +0200

    Add todo to check missing dependency for React

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import styles from './index.css';
+//TODO: we don't use React in code, but we need it somehow.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
