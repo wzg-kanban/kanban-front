f6ba08fc61d2fc141baedffc98af363d65bf25f9
kiper <kacgrzes@gmail.com>
2016-08-28 15:18:54 +0200

    Refactor Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-export default () => <h1>Welcome home! <Link to="/app">Go</Link> to app</h1>;
+export default () =>
+    <h1>Welcome home!
+        <Link to="/app">Go</Link> to app
+    </h1>;
