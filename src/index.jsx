014f89e39413d130fdc41aea07b9230ea70c43eb
kiper <kacgrzes@gmail.com>
2016-08-28 02:57:25 +0200

    Add Home screen

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import Provider from 'components/Provider';
 import { Router, Route, hashHistory } from 'react-router';
+import Home from 'screens/Home';
 
 ReactDOM.render(
     <Provider>
         <Router history={hashHistory}>
-            <Route path="/" component={App}/>
+            <Route path="/" component={Home}/>
+            <Route path="/app" component={App}/>
         </Router>
     </Provider>,
     document.getElementById('app')
